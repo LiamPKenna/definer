3b617f239338187f5decc3ca8cd111be4464c248
LiamPKenna <liampkenna@gmail.com>
2019-12-20 09:45:44 -0800

    Add initialize method to Definition

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,5 +1,15 @@
 class Definition
-  def initialize
+  attr_reader :id, :word_id
+  attr_accessor :text
 
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @text = attributes.fetch(:text)
+    @word_id = attributes.fetch(:word_id)
+    @id = (attributes.fetch(:id)) ? attributes.fetch(:id) : @@total_rows += 1
   end
+
+  
 end
