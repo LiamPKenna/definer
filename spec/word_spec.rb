ec4dcc89ff1282e7e9788ed2f323428a73a44b4d
LiamPKenna <liampkenna@gmail.com>
2019-12-20 09:08:31 -0800

    Add spec for Word.find class method and change definition to definitions

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,15 +8,9 @@ describe('#Word') do
     Word.clear
   end
 
-  describe('.all') do
-    it('returns an empty array before any words have been added') do
-      expect(Word.all).to(eq([]))
-    end
-  end
-
   describe('#save') do
     it('saves a word') do
-      word = Word.new({:word=>'word', :definition=>nil, :id=>nil})
+      word = Word.new({:word=>'word', :definitions=>nil, :id=>nil})
       word.save
       expect(Word.all).to(eq([word]))
     end
@@ -24,20 +18,34 @@ describe('#Word') do
 
   describe('#==') do
     it('considers words with matching attributes equal') do
-      word = Word.new({:word=>'word', :definition=>nil, :id=>nil})
-      word2 = Word.new({:word=>'word', :definition=>nil, :id=>nil})
+      word = Word.new({:word=>'word', :definitions=>nil, :id=>nil})
+      word2 = Word.new({:word=>'word', :definitions=>nil, :id=>nil})
       expect(word2).to(eq(word))
+    end
+  end
+
+  describe('.all') do
+    it('returns an empty array before any words have been added') do
+      expect(Word.all).to(eq([]))
     end
   end
 
   describe('.clear') do
     it('removes all words from the class variable @@words') do
-      word = Word.new({:word=>'word', :definition=>nil, :id=>nil})
+      word = Word.new({:word=>'word', :definitions=>nil, :id=>nil})
       word.save
-      word2 = Word.new({:word=>'word2', :definition=>nil, :id=>nil})
+      word2 = Word.new({:word=>'word2', :definitions=>nil, :id=>nil})
       word2.save
       Word.clear()
       expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it('returns a word matching a given id number') do
+      word = Word.new({:word=>'word', :definitions=>nil, :id=>nil})
+      word.save
+      expect(Word.find(word.id)).to(eq(word))
     end
   end
 
