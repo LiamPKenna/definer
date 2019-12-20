fb2a20f8e04d39c12bb1b1832fedf13b571b5186
LiamPKenna <liampkenna@gmail.com>
2019-12-20 13:49:15 -0800

    Add integration test for updating a word

--- spec/definer_integration_spec.rb
+++ spec/definer_integration_spec.rb
@@ -24,3 +24,18 @@ describe('delete a word', {:type => :feature}) do
     expect(page).to have_no_content('DeleteMe')
   end
 end
+
+describe('update a word', {:type => :feature}) do
+  it('should allow for users to update added words') do
+    visit('/words')
+    click_on('ADD A NEW WORD!')
+    fill_in('input_word', :with => 'UptateMe')
+    click_on('Go!')
+    expect(page).to have_content('UptateMe')
+    page.find('button', id: 'updateUptateMe').click
+    fill_in('input_word', :with => 'IwasUpdated')
+    click_on('Go!')
+    expect(page).to have_content('IwasUpdated')
+    expect(page).to have_no_content('UptateMe')
+  end
+end
