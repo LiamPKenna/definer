e322e0b9cb4aa419e0337cfbd1c41e31e756d71f
LiamPKenna <liampkenna@gmail.com>
2019-12-20 14:11:49 -0800

    Add integration test for adding definitions

--- spec/definer_integration_spec.rb
+++ spec/definer_integration_spec.rb
@@ -39,3 +39,17 @@ describe('update a word', {:type => :feature}) do
     expect(page).to have_no_content('UptateMe')
   end
 end
+
+describe('add a new definition', {:type => :feature}) do
+  it('should allow for users to input definitions for words') do
+    visit('/words')
+    click_on('ADD A NEW WORD!')
+    fill_in('input_word', :with => 'AddDefinition')
+    click_on('Go!')
+    click_on('AddDefinition')
+    click_on('ADD A NEW DEFINITION!')
+    fill_in('input_definition', :with => 'Test definition')
+    click_on('Go!')
+    expect(page).to have_content('Test definition')
+  end
+end
