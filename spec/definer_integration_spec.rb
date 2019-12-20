948bd2cb248c59b9c0a977409518fd43b66a4d99
LiamPKenna <liampkenna@gmail.com>
2019-12-20 13:45:30 -0800

    Add integration test for deleting words

--- spec/definer_integration_spec.rb
+++ spec/definer_integration_spec.rb
@@ -3,7 +3,7 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('app.rb', {:type => :feature}) do
+describe('add a new word', {:type => :feature}) do
   it('should allow for users to input words') do
     visit('/words')
     click_on('ADD A NEW WORD!')
@@ -11,5 +11,16 @@ describe('app.rb', {:type => :feature}) do
     click_on('Go!')
     expect(page).to have_content('Testerino')
   end
+end
 
+describe('delete a word', {:type => :feature}) do
+  it('should allow for users to delete added words') do
+    visit('/words')
+    click_on('ADD A NEW WORD!')
+    fill_in('input_word', :with => 'DeleteMe')
+    click_on('Go!')
+    expect(page).to have_content('DeleteMe')
+    page.find('button', id: 'deleteDeleteMe').click
+    expect(page).to have_no_content('DeleteMe')
+  end
 end
