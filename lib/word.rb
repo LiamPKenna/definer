4b36cacaf001dbdcb43d9d023e8d9f4d197858f7
LiamPKenna <liampkenna@gmail.com>
2019-12-20 08:32:35 -0800

    Add Word.all method to return all words in the class variable

--- lib/word.rb
+++ lib/word.rb
@@ -1,5 +1,15 @@
 class Word
-  def initialize
+  attr_reader :word, :id
+  @@words = {}
+  @@total_rows = 0
 
+  def initialize
+    @word = word
+    @id = (id) ? id : @@total_rows += 1
   end
+
+  def self.all
+    @@words.values()
+  end
+
 end
