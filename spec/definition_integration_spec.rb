a5db57fbdfb28cdeada74f72e0371d86f2958785
LiamPKenna <liampkenna@gmail.com>
2019-12-20 15:07:48 -0800

    Split integration psecs into two class specific files

--- spec/definition_integration_spec.rb
+++ spec/definition_integration_spec.rb
@@ -3,43 +3,6 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('add a new word', {:type => :feature}) do
-  it('should allow for users to input words') do
-    visit('/words')
-    click_on('ADD A NEW WORD!')
-    fill_in('input_word', :with => 'Testerino')
-    click_on('Go!')
-    expect(page).to have_content('Testerino')
-  end
-end
-
-describe('delete a word', {:type => :feature}) do
-  it('should allow for users to delete added words') do
-    visit('/words')
-    click_on('ADD A NEW WORD!')
-    fill_in('input_word', :with => 'DeleteMe')
-    click_on('Go!')
-    expect(page).to have_content('DeleteMe')
-    page.find('button', id: 'deleteDeleteMe').click
-    expect(page).to have_no_content('DeleteMe')
-  end
-end
-
-describe('update a word', {:type => :feature}) do
-  it('should allow for users to update added words') do
-    visit('/words')
-    click_on('ADD A NEW WORD!')
-    fill_in('input_word', :with => 'UptateMe')
-    click_on('Go!')
-    expect(page).to have_content('UptateMe')
-    page.find('button', id: 'updateUptateMe').click
-    fill_in('input_word', :with => 'IwasUpdated')
-    click_on('Go!')
-    expect(page).to have_content('IwasUpdated')
-    expect(page).to have_no_content('UptateMe')
-  end
-end
-
 describe('add a new definition', {:type => :feature}) do
   it('should allow for users to input definitions for words') do
     visit('/words')
