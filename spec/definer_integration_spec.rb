da8742ebede9ce24900ed747583640931309bea4
LiamPKenna <liampkenna@gmail.com>
2019-12-20 13:16:55 -0800

    Add integration test for input of words

--- spec/definer_integration_spec.rb
+++ spec/definer_integration_spec.rb
@@ -1,13 +1,15 @@
-# require('rspec')
-# require('./app')
-# require('capybara')
-#
-# describe('#Integration') do
-#   before(:each) do
-#   end
-#   describe('#') do
-#     it('') do
-#       expect().to(eq())
-#     end
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('app.rb', {:type => :feature}) do
+  it('should allow for users to input words') do
+    visit('/words')
+    click_on('ADD A NEW WORD!')
+    fill_in('input_word', :with => 'Testerino')
+    click_on('Go!')
+    expect(page).to have_content('Testerino')
+  end
+
+end
