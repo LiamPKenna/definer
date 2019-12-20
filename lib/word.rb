0551ae150a6bd5ef21f1eb7efc5ff9d4d4c2fb36
LiamPKenna <liampkenna@gmail.com>
2019-12-20 08:47:58 -0800

    Add Word#save method that fails spec due to lack of == method

--- lib/word.rb
+++ lib/word.rb
@@ -1,15 +1,24 @@
 class Word
-  attr_reader :word, :id
+  attr_reader :word, :id, :definition
   @@words = {}
   @@total_rows = 0
 
-  def initialize
-    @word = word
-    @id = (id) ? id : @@total_rows += 1
+  def initialize(attributes)
+    @word = attributes.fetch(:word)
+    @definition = attributes.fetch(:definition)
+    @id = (attributes.fetch(:id)) ? attributes.fetch(:id) : @@total_rows += 1
   end
 
   def self.all
     @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new({
+      :word=>self.word,
+      :definition=>self.definition,
+      :id=>self.id
+    })
   end
 
 end
