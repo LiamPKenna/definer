174ab74733655c23e1c331da580b92ab273221a8
LiamPKenna <liampkenna@gmail.com>
2019-12-20 09:35:51 -0800

    Add spec for Definition.all

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,13 +1,23 @@
-# require('rspec')
-# require('definition')
-#
-#
-# describe('#Definition') do
-#   before(:each) do
-#   end
-#   describe('#') do
-#     it('') do
-#       expect().to(eq())
-#     end
-#   end
-# end
+require('rspec')
+require('definition')
+require('word')
+
+describe('#Definition') do
+
+  # before(:each) do
+  #
+  # end
+
+  describe('.all') do
+    it('returns an empty array before any definitions have been added') do
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
+  # describe('#') do
+  #   it('') do
+  #     expect().to(eq())
+  #   end
+  # end
+
+end
