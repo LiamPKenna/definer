59732831882041d4558af7ed0f5b2eb40d064d29
LiamPKenna <liampkenna@gmail.com>
2019-12-20 09:14:29 -0800

    Add .spec, comment integration/definition specs, correct require in app

--- spec/definer_integration_spec.rb
+++ spec/definer_integration_spec.rb
@@ -1,13 +1,13 @@
-require('rspec')
-require('integration')
-require('capybara')
-
-describe('#Integration') do
-  before(:each) do
-  end
-  describe('#') do
-    it('') do
-      expect().to(eq())
-    end
-  end
-end
+# require('rspec')
+# require('./app')
+# require('capybara')
+#
+# describe('#Integration') do
+#   before(:each) do
+#   end
+#   describe('#') do
+#     it('') do
+#       expect().to(eq())
+#     end
+#   end
+# end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,13 +1,13 @@
-require('rspec')
-require('definition')
-
-
-describe('#Definition') do
-  before(:each) do
-  end
-  describe('#') do
-    it('') do
-      expect().to(eq())
-    end
-  end
-end
+# require('rspec')
+# require('definition')
+#
+#
+# describe('#Definition') do
+#   before(:each) do
+#   end
+#   describe('#') do
+#     it('') do
+#       expect().to(eq())
+#     end
+#   end
+# end
